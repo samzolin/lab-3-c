2f6b8bf8c96887b9a45a18e5dc51d095b245b59e
samzolin <saz5193@psu.edu>
2020-09-16 15:12:55 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+//Author: Samantha Zolin saz5193@psu.edu
+//Collaborator: 
+//Collaborator: 
+//Collaborator: 
+//Section: 10
+//Break out: 
+#include <stdio.h>
+#include <math.h>
+#include <readline/readline.h>
+#include <string.h>
+#include <stdlib.h>
+
+int sum_n(int n); 
+  if (n <= 0):
+  return ;
+  
+
+
+  
