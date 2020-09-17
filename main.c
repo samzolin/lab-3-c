0e065541d5f1dd194fa511e41addb8e9c942fe22
samzolin <saz5193@psu.edu>
2020-09-17 19:32:13 +0000

    add line

--- main.c
+++ main.c
@@ -1,18 +1,23 @@
 //Author: Samantha Zolin saz5193@psu.edu
-//Collaborator: 
-//Collaborator: 
-//Collaborator: 
+//Collaborator: Max Simpson mes6581@psu.edu
+//Collaborator: Matthew Frabizio mjf6328@psu.edu
+//Collaborator: Nathan Donahue ndd5167@psu.edu
 //Section: 10
-//Break out: 
+//Break out: 13
 #include <stdio.h>
 #include <math.h>
 #include <readline/readline.h>
 #include <string.h>
 #include <stdlib.h>
 
-int sum_n(int n); 
-  if (n <= 0):
-  return ;
+int sum_n(int n) {
+  if (n <= 1) {
+    return n;
+  }
+  else {
+    return n+sum_n(n-1);
+  }
+}
   
 
 
